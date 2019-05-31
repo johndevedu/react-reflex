f5506f050273cbe6c1c5ce045d7f6b51a3cfab90
John <johnwoolee@gmail.com>
2019-05-31 10:03:21 -0700

    add min width

--- src/index.js
+++ src/index.js
@@ -2,36 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "react-reflex/styles.css";
-import {
-  ReflexContainer,
-  ReflexElement,
-  ReflexSplitter,
-  ReflexHandle
-} from "react-reflex";
+import { ReflexContainer, ReflexElement, ReflexSplitter } from "react-reflex";
 
 import "./styles.css";
 
 class App extends React.Component {
+  handleResize = ({ domElement, component }) => {
+    console.log(domElement);
+    debugger;
+    setTimeout(() => (domElement.style.width = "10px"));
+  };
   render() {
     return (
       <ReflexContainer style={{ height: "100vh" }} orientation="vertical">
-        <ReflexElement className="left-pane">
+        <ReflexElement className="left-pane" minSize="0">
           <div className="pane-content">
             <label>Left Pane (resizable)</label>
           </div>
         </ReflexElement>
 
         <ReflexSplitter
+          propagate={true}
           style={{
             backgroundColor: "#ccdff2",
             textAlign: "center",
             width: "15px"
           }}
         >
-          <div class="splitter">o o o</div>
+          <div className="splitter">o o o</div>
         </ReflexSplitter>
 
-        <ReflexElement className="right-pane" minSize="200" maxSize="800">
+        <ReflexElement
+          className="right-pane"
+          minSize="0"
+          onStopResize={this.handleResize}
+        >
           <div className="pane-content">
             <div style={{ display: "flex" }}>
               <label>
